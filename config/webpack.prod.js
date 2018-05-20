ef918f668dd37994e26556311bea482e9c604757
drunkbug <qiang.l@husky.neu.edu>
2018-05-19 21:59:59 -0400

    Webpack production config for api end point

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,6 +6,8 @@ const merge = require('webpack-merge')
 
 const serverURL = process.env['SERVER_URL']
 const appURL = process.env['APP_URL']
+const api_end_point = process.env['API_END_POINT']
+
 
 module.exports = merge(common, {
   mode: 'production',
@@ -17,6 +19,7 @@ module.exports = merge(common, {
       'process.env': {
         'SERVER_URL': JSON.stringify(serverURL),
         'APP_URL': JSON.stringify(appURL),
+        'API_END_POINT': JSON.stringify(api_end_point),
       },
     }),
   ],
